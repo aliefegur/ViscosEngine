9bcdf0f65cfc094ad5aa3cc8de2b1424ce66ddf0
aliefegur <ali.efe.gur@hotmail.com>
2026-09-15 19:35:56 +0300

    Logging Format Updated

--- ViscosEngine/Viscos/Core/Log.cpp
+++ ViscosEngine/Viscos/Core/Log.cpp
@@ -9,12 +9,12 @@ namespace Viscos {
 
 	void Log::Initialize() 
 	{
-		spdlog::set_pattern("%^[%T] %n: %v%$");
+		spdlog::set_pattern("%^[%T] [%n] %v%$");
 
-		s_CoreLogger = spdlog::stdout_color_mt("Viscos Core");
+		s_CoreLogger = spdlog::stdout_color_mt("CORE");
 		s_CoreLogger->set_level(spdlog::level::trace);
 
-		s_ClientLogger = spdlog::stdout_color_mt("Application");
+		s_ClientLogger = spdlog::stdout_color_mt("APP");
 		s_ClientLogger->set_level(spdlog::level::trace);
 
 		VSCS_CORE_TRACE("Logging system initialized!");
