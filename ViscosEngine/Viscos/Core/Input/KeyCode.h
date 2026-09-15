7a27cd8d3e676e873b9f78ca8241d8e4122a7af2
aliefegur <ali.efe.gur@hotmail.com>
2026-09-16 00:42:10 +0300

    KeyCodes

---
+++ ViscosEngine/Viscos/Core/Input/KeyCode.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <cstdint>
+
+namespace Viscos
+{
+	enum class KeyCode : uint16_t
+	{
+		Unknown = 0,
+		Space, Apostrophe, Comma, Minus, Period, Slash,
+		D0, D1, D2, D3, D4, D5, D6, D7, D8, D9,
+		Semicolon, Equal,
+        A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z,
+		LeftBracket, Backslash, RightBracket, GraveAccent,
+		Escape, Enter, Tab, Backspace, Insert, Delete,
+		Right, Left, Down, Up,
+        PageUp, PageDown, Home, End,
+        CapsLock, ScrollLock, NumLock,
+        PrintScreen, Pause,
+        F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12,
+        LeftShift, LeftControl, LeftAlt, LeftSuper,
+        RightShift, RightControl, RightAlt, RightSuper,
+        Menu,
+		Count
+	};
+
+	constexpr uint16_t KeyCodeCount = static_cast<uint16_t>(KeyCode::Count);
+}
