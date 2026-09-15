9982b434735fdd6e78b5d9dc536423367eb4409a
aliefegur <ali.efe.gur@hotmail.com>
2026-09-15 19:23:32 +0300

    Precompiled Header

---
+++ ViscosEngine/viscos_pch.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifdef VISCOS_PLATFORM_WINDOWS
+#include <Windows.h>
+#endif
+
+#include <string>
+#include <iostream>
+#include <ostream>
+#include <sstream>
+#include <algorithm>
+#include <memory>
+#include <cstdlib>
+#include <cstdint>
+#include <filesystem>
